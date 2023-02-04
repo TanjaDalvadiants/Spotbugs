50115cab9f2b2da9791762e3ad59aa4b5a72552b
tatiana dalvadiants <eparis@atlassian.com>
2023-02-04 15:28:26 +0400

    OldFileAdded

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+
+    long amount = 100_060;
+    boolean registered = true;
+
+
+    long actual = service.calculate (amount, registered);
+
+  }
+}
+
